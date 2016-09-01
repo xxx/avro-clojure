994c1e22da238c9ca49c71942b0d1853403eaefb
mpd <mpd@jesters-court.net>
2016-09-01 14:10:08 -0700

    stuff

---
+++ src/avro_clojure/core.clj
@@ -0,0 +1,24 @@
+(ns avro-clojure.core
+  (:require [abracad.avro :as avro]
+            [clojure.java.shell :refer [sh]]))
+
+(defn parse-schema-filepath
+  [schema-file]
+  (let [f (slurp schema-file)]
+    (avro/parse-schema f)))
+
+(defn write-employee
+  [employee output-filename]
+  (let [schema (parse-schema-filepath "schema/mpd-simple.avsc")]
+    (with-open [adf (avro/data-file-writer "snappy" schema output-filename)]
+      (.append adf {:Name (:name employee) :Age (:age employee)})
+      (.append adf {:Name "a dog" :Age 4}))))
+
+(defn read-employee
+  [filename]
+  (with-open [adf (avro/data-file-reader filename)]
+    (doall (seq adf))))
+
+(comment
+  (write-employee {:name "michael dungeon" :age 666} "mpd.out")
+  (read-employee "mpd.out"))
